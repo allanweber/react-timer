8db4f5d8bdc616c35b54377be13d0013015ee9d3
Allan Weber <a.cassianoweber@gmail.com>
2020-06-20 08:39:04 +0200

    First Version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import { ReactComponent as Play } from "./assets/play.svg";
+import { ReactComponent as Pause } from "./assets/pause.svg";
 
-function App() {
+//Add keyboard supports for space bar, enter, direction, scroll above the number
+// Add sound for tick and end time
+
+const App = () => {
+  const [timer, setTimer] = useState(3);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    const timerTick = () => {
+      if (running && timer > 0) {
+        setTimer(timer - 1);
+      }
+      if (timer === 1) {
+        setRunning(false);
+      }
+    };
+    const interval = setInterval(timerTick, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timer, running]);
+
+  const formatTimeLeft = () => {
+    const minutes = Math.floor(timer / 60);
+    let seconds = timer % 60;
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    return `${minutes}:${seconds}`;
+  };
+
+  const toggleRunning = () => {
+    setRunning(!running);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>{formatTimeLeft()}</h1>
+      <div className="buttons">
+        <button className={running ? "isRunning" : ""} onClick={toggleRunning}>
+          {running ? <Pause /> : <Play />}
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
