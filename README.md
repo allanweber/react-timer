fbc79d80220c68a80342c159360cc4f66c066ca3
Allan Weber <a.cassianoweber@gmail.com>
2020-06-20 09:44:58 +0200

    add sound support

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # React Timer
 
 <img src="Screenshot.png">
+
+## Heroku CLI Commands
+
+heroku create allan-react-timer --buildpack http://github.com/mars/create-react-app-buildpack.git
+git push heroku master
--- src/App.js
+++ src/App.js
@@ -1,31 +1,62 @@
 import React, { useState, useEffect } from "react";
+import useSound from "use-sound";
+
 import logo from "./logo.svg";
 import "./App.scss";
 import { ReactComponent as Play } from "./assets/play.svg";
 import { ReactComponent as Pause } from "./assets/pause.svg";
+import switchOff from "./assets/switch-off.mp3";
+import fanfare from "./assets/fanfare.mp3";
 
 //Add keyboard supports for space bar, enter, direction, scroll above the number
-// Add sound for tick and end time
 
 const App = () => {
-  const [timer, setTimer] = useState(3);
+  const [timer, setTimer] = useState(7);
   const [running, setRunning] = useState(false);
+  const [finishing] = useSound(switchOff);
+  const [finished] = useSound(fanfare);
 
   useEffect(() => {
     const timerTick = () => {
-      if (running && timer > 0) {
-        setTimer(timer - 1);
+      if (running) {
+        if (timer > 0) {
+          setTimer(timer - 1);
+        }
       }
-      if (timer === 1) {
+      if (timer === 0) {
         setRunning(false);
       }
     };
-    const interval = setInterval(timerTick, 1000);
 
+    const interval = setInterval(timerTick, 1000);
     return () => {
       clearInterval(interval);
     };
   }, [timer, running]);
+
+  useEffect(() => {
+    const playFinishing = () => {
+      if (running) {
+        if (timer <= 5 && timer > 1) {
+          finishing();
+        }
+      }
+    };
+
+    const interval = setInterval(playFinishing, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timer, running, finishing]);
+
+  useEffect(() => {
+    const playFinish = () => {
+      if (timer === 0) {
+        finished();
+      }
+    };
+    playFinish();
+  }, [timer, finished]);
 
   const formatTimeLeft = () => {
     const minutes = Math.floor(timer / 60);
